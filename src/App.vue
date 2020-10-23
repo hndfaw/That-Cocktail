c39778abfa8458eda019a00ccff7e62b334d66dc
“Hindreen” <hndfaw@gmail.com>
2020-10-23 11:55:03 -0400

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+          <NavBar  className="nav-bar"/>
     <div id="nav">
+
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -8,13 +10,27 @@
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar
+  }
+};
+</script>
+
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding-top: 50px;
 }
 
 #nav {
